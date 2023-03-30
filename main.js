6d014ba75c9d3b68d74963222dcbf9d4bbd83be4
Your Name <you@example.com>
2023-03-30 16:07:42 +0200

    Array STOPS definiert und angezeigt

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ let stop_lng = 172.636667;
 let zoom = 13;
 let title = 'Christchurch';
 
+const STOPS = ["Auckland", "Christchurch", "Lake Tekapo"];
+console.log (STOPS)
+console.log (STOPS[0]);
+console.log (STOPS[1]);
+console.log (STOPS[2]);
+console.log (STOPS.length);
+
+
 let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
