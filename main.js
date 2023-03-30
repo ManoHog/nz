ae610b13f41c42dee8e149a71a5e767f2660eb7f
Your Name <you@example.com>
2023-03-30 15:53:06 +0200

    Variable stop_lat, lng, title und zoom eingefügt

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-var map = L.map('map').setView([-43.531111, 172.636667], 13);
+let stop_lat = -43.531111;
+let stop_lng = 172.636667;
+let zoom = 13;
+let title = 'Christchurch';
+
+let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
-    L.marker([-43.531111, 172.636667]).addTo(map)
-        .bindPopup('Christchurch')
+    L.marker([stop_lat, stop_lng]).addTo(map)
+        .bindPopup(title)
         .openPopup();
         
